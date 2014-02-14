8229d2a9f2216240af4ad278a53cdb72756dd279
steve piantadosi <spiantado@gmail.com>
2014-02-14 15:45:08 -0800

    fix

--- LOTlib/FunctionNode.py
+++ LOTlib/FunctionNode.py
@@ -369,32 +369,32 @@ class FunctionNode(object):
 			g.setto(copy(replace))
 
 	
-	def iter_partial_subtrees(self):
-		"""
-			Iterator for all partial subtrees (may end at a nonterminal)
-			as opposed to the self.__iter__ that interates over complete subtrees, ending
-			only with terminals.
+	#def iter_partial_subtrees(self):
+		#"""
+			#Iterator for all partial subtrees (may end at a nonterminal)
+			#as opposed to the self.__iter__ that interates over complete subtrees, ending
+			#only with terminals.
 			
-			NOTE: There are LOTS more of these than full subtrees!
-		"""
-		#argfn = filter(isFunctionNode, x.args)
+			#NOTE: There are LOTS more of these than full subtrees!
+		#"""
+		##argfn = filter(isFunctionNode, x.args)
 		
-		def itermark(x):
-			""" Return True when we carry"""
+		#def itermark(x):
+			#""" Return True when we carry"""
 			
-			if x.marked:
-				x.marked = False
+			#if x.marked:
+				#x.marked = False
 				
-				afn = filter(isFunctionNode, x.args)
+				#afn = filter(isFunctionNode, x.args)
 				
-				for i, a in enumerate(afn):
-					if itermark(a): break
-			else:
-				x.marked = True
-				return False
+				#for i, a in enumerate(afn):
+					#if itermark(a): break
+			#else:
+				#x.marked = True
+				#return False
 					
 		
-		for t in self:
+		#for t in self:
 			
 			
 			
