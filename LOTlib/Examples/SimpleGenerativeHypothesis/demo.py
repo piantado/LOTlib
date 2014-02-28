1468b55a011f44d8320a66b0dbf1b1c3a24d1ebb
steve piantadosi <spiantado@gmail.com>
2014-02-27 17:56:54 -0800

    added Simple Generative hypothesis

---
+++ LOTlib/Examples/SimpleGenerativeHypothesis/demo.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""
+
+	 A demo of "syntax" learning using a SimpleGenerativeHypothesis.
+	 
+	 This searches over probabilistic generating functions, running them forward to estimate
+	 the likelihood of the data. Very very simple. 
+"""
+
+from LOTlib.Grammar import Grammar
+from LOTlib.BasicPrimitives import *
+from LOTlib.Inference.MetropolisHastings import mh_sample
+from LOTlib.Miscellaneous import *
+from LOTlib.Hypotheses.SimpleGenerativeHypothesis import SimpleGenerativeHypothesis
+
+NDATA = 50 # How many of each data point have we seen?
+
+data = {
+        'N V'       : NDATA,
+        'D N V'     : NDATA,
+        'D N V N'   : NDATA,
+        'D N V D N' : NDATA,         
+}
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+TERMINAL_WEIGHT = 5.
+
+G = Grammar()
+G.add_rule('START', 'flatten2str', ['EXPR'], 1.0)
+
+G.add_rule('BOOL', 'and_', ['BOOL', 'BOOL'], 1.)
+G.add_rule('BOOL', 'or_', ['BOOL', 'BOOL'], 1.)
+G.add_rule('BOOL', 'not_', ['BOOL'], 1.)
+
+G.add_rule('EXPR', 'if_', ['BOOL', 'EXPR', 'EXPR'], 1.)
+G.add_rule('BOOL', 'equal_', ['EXPR', 'EXPR'], 1.)
+
+G.add_rule('BOOL', 'flip_', [''], TERMINAL_WEIGHT)
+
+# List-building operators
+G.add_rule('EXPR', 'cons_', ['EXPR', 'EXPR'], 1.)
+G.add_rule('EXPR', 'cdr_', ['EXPR'], 1.)
+G.add_rule('EXPR', 'car_', ['EXPR'], 1.)
+
+G.add_rule('EXPR', '[]', None, TERMINAL_WEIGHT)
+G.add_rule('EXPR', q('D'), None, TERMINAL_WEIGHT)
+G.add_rule('EXPR', q('A'), None, TERMINAL_WEIGHT)
+G.add_rule('EXPR', q('N'), None, TERMINAL_WEIGHT)
+G.add_rule('EXPR', q('V'), None, TERMINAL_WEIGHT)
+G.add_rule('EXPR', q('who'), None, TERMINAL_WEIGHT)
+
+
+## Allow lambda abstraction
+G.add_rule('EXPR', 'apply_', ['LAMBDAARG', 'LAMBDATHUNK'], 1)
+G.add_rule('LAMBDAARG',   'lambda', ['EXPR'], 1., bv_type='EXPR', bv_args=[] )
+G.add_rule('LAMBDATHUNK', 'lambda', ['EXPR'], 1., bv_type=None, bv_args=None ) # A thunk
+
+
+#(lambda x. (.... (x) )
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+from LOTlib.Proposals import *
+
+h0 = SimpleGenerativeHypothesis(G )
+
+## populate the finite sample by running the sampler for this many steps
+for h in mh_sample(h0, data, 100000, skip=100):
+	print h.posterior_score, h.prior, h.likelihood, h
+	print h.llcounts
+	
+        
---
+++ LOTlib/Examples/SymbolicRegression/MAPSymbolicRegression.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+	Symbolic regression that fits parameters using a MAP estimate of the continuous parameters.
+	We put a simple bayesian prior on these constants, and use it to compute MAPs
+"""
+
+from Shared import * # imports G
+from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
+import scipy
+import numpy
+from scipy.optimize import fmin
+from numpy.random import normal
+from math import sin
+
+NCONSTANTS = 4
+CONSTANT_NAMES = ['C%i'%i for i in xrange(NCONSTANTS) ] 
+CONSTANT_SD = 1.0
+
+CHAINS = 1
+STEPS = 50000
+SKIP = 0
+LL_SD = 0.1 # the SD of the likelihood
+NDATA = 50
+
+## The target function for symbolic regression 
+target = lambda x: 3.*x + sin(4.3/x)
+
+# Supplement the grammar
+for c in CONSTANT_NAMES: G.add_rule('EXPR', c, None, 5.0)
+
+
+class MAPSymbolicRegressionHypothesis(GaussianLOTHypothesis):
+	"""
+		This is a quick hack to try out symbolic regression with constants just fit. 
+		This hacks it by defining a self.CONSTANT_VALUES that are automatically read from 
+		get_function_responses (overwritten). We can then change them and repeatedly compute the 
+		likelihood to optimize
+		
+	"""
+	
+	def value2function(self, value):
+		"""
+		Overwrite this from FunctionHypothesis. Here, we add args for the constants so we can use them
+		"""
+		return evaluate_expression(value, args=self.args+CONSTANT_NAMES)
+	
+	
+	def get_function_responses(self, data, *args, **kwargs):
+		return GaussianLOTHypothesis.get_function_responses(self, data, *self.CONSTANT_VALUES) # Pass in the constant names
+	
+	def compute_likelihood(self, data):
+		
+		def llgivenC(fit_params):
+			self.CONSTANT_VALUES = fit_params.tolist() # set these
+			# And return the original likelihood, which by get_function_responses above uses this
+			constant_prior = sum(map(lambda x: normlogpdf(x,0.0,CONSTANT_SD), self.CONSTANT_VALUES))
+			return -(GaussianLOTHypothesis.compute_likelihood(self, data) + constant_prior)
+
+		while True:
+			p0 = normal(0.0, CONSTANT_SD, NCONSTANTS)
+			res = fmin(llgivenC, p0, disp=False)
+			if llgivenC(res) < Infinity: break
+		
+		self.likelihood = -llgivenC(res) ## must invert since it's a negative
+		self.CONSTANT_VALUES = res
+		
+		self.posterior_score = self.prior + self.likelihood
+		return self.likelihood
+
+
+
+# Make up some learning data for the symbolic regression
+def generate_data(data_size):
+	
+	# initialize the data
+	data = []
+	for i in range(data_size): 
+		x = random()
+		y = target(x) + normal()*LL_SD
+		data.append( FunctionData(input=[x], output=y, ll_sd=LL_SD) )
+	
+	return data
+	
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# starting hypothesis -- here this generates at random
+
+data = generate_data(NDATA) # generate some data
+h0 = MAPSymbolicRegressionHypothesis(G)
+h0.CONSTANT_VALUES = numpy.zeros(NCONSTANTS)
+
+for h in mh_sample(h0, data, STEPS, skip=SKIP, trace=F, debug=F):
+	print h.posterior_score, h.likelihood, h.prior, h.CONSTANT_VALUES, qq(h)
+	pass
+	
