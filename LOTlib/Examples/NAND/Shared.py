5790a9638319d2d17fd1d9f3d1b69011b9dbb680
J. Hassler Thurston <jthurst3@u.rochester.edu>
2014-07-10 15:26:09 -0400

    remove some backslashes, in middle of bisecting revision tree to find which commit will successfully run DoInference.py

--- LOTlib/Examples/NAND/Shared.py
+++ LOTlib/Examples/NAND/Shared.py
@@ -14,12 +14,12 @@ COLORS = ['blue', 'red', 'green']
 # Some concepts to try to learn
 # ------------------------------------------------------------------
 
-TARGET_CONCEPTS = [lambda x: and_(is_shape_(x,'square'), is_color_(x,'blue')), \
-	    lambda x: or_(is_shape_(x,'triangle'), is_color_(x,'green')), \
-	    lambda x: or_(is_shape_(x,'square'), is_color_(x,'red')), \
-	    lambda x: and_(not_(is_shape_(x,'rectangle')), is_color_(x,'red')), \
-	    lambda x: and_(not_(is_shape_(x,'square')), not_(is_color_(x,'blue'))), \
-	    lambda x: and_(is_shape_(x,'rectangle'), is_color_(x,'green')), \
+TARGET_CONCEPTS = [lambda x: and_(is_shape_(x,'square'), is_color_(x,'blue')), 
+	    lambda x: or_(is_shape_(x,'triangle'), is_color_(x,'green')), 
+	    lambda x: or_(is_shape_(x,'square'), is_color_(x,'red')), 
+	    lambda x: and_(not_(is_shape_(x,'rectangle')), is_color_(x,'red')), 
+	    lambda x: and_(not_(is_shape_(x,'square')), not_(is_color_(x,'blue'))), 
+	    lambda x: and_(is_shape_(x,'rectangle'), is_color_(x,'green')), 
 	    lambda x: or_(not_(is_shape_(x,'triangle')), is_color_(x,'red')) ]
 
 # ------------------------------------------------------------------
