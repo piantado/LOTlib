97636de34b509f5e2600117c556de4f44991ac41
steve piantadosi <spiantado@gmail.com>
2014-07-19 21:47:45 -0400

    fixed errors in ParallelTempering

--- LOTlib/Examples/Number/plot-evaluation.R
+++ LOTlib/Examples/Number/plot-evaluation.R
@@ -6,6 +6,6 @@ library(ggplot2)
 # d <- read.table("evaluation-temperatures-aggregate.txt")
 d <- read.table("ei.txt")
 
-p <- ggplot(d, aes(x=V3, y=V5, color=as.character(V2))) + stat_summary(fun.y=median, geom="line", size=2)
+p <- ggplot(d, aes(x=V3, y=V5, color=as.character(V2))) + stat_summary(fun.y=mean, geom="line", size=2)
 
 p
