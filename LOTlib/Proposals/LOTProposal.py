0c9939193ad275ee708d677ba7d553aa732386ac
Steven Piantadosi <spiantado@gmail.com>
2014-08-01 13:32:31 -0400

    Added Proposals

---
+++ LOTlib/Proposals/LOTProposal.py
@@ -0,0 +1,19 @@
+from copy import copy
+
+class LOTProposal(object):
+	"""
+		A class of LOT proposals. This wraps calls with copying of the hypothesis
+		so that we can implement only propose_t classes for subclasses, that generate trees
+	"""
+	def __init__(self, grammar):
+		self.__dict__.update(locals())
+		
+	def __call__(self, h, **kwargs):
+		# A wrapper that calls propose_tree (defined in subclasses) on our tree value
+		# so this manages making LOTHypotheses (or the relevant subclass), and proposal subclasses
+		# can just manage trees
+		p = copy(h)
+		ret = self.propose_tree(h.value, **kwargs) # don't unpack, since we may return [newt,fb] or [newt,f,b]
+		p.set_value(ret[0])
+		ret[0] = p
+		return ret
---
+++ LOTlib/Proposals/MixtureProposal.py
@@ -0,0 +1,24 @@
+from LOTProposal import LOTProposal
+import numpy
+from LOTlib.Miscellaneous import weighted_sample
+
+class MixtureProposal(LOTProposal):
+	"""
+		A mixture of proposals, like
+		
+		m = MixtureProposal([RegenerationProposal(grammar), InsertDeleteProposal(grammar)] )
+		
+		Probabilities of each can be specified
+	
+	"""
+	def __init__(self, proposals, probs=None):
+		#print proposals
+		self.__dict__.update(locals())
+		
+		if probs is None:
+			self.probs = numpy.array( [1.] * len(proposals) )
+		
+	def propose_tree(self, t):
+		p = weighted_sample(self.proposals, probs=self.probs, log=False)
+		
+		return p.propose_tree(t)
