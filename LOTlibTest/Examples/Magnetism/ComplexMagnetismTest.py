000ff5ae70654c6c47d49008769dfd1a385e9dd5
sa- <samay.kapadia@gmail.com>
2014-08-01 15:21:40 -0400

    LOTlibTest should now be added in

---
+++ LOTlibTest/Examples/Magnetism/ComplexMagnetismTest.py
@@ -0,0 +1,34 @@
+"""
+class to test ComplexMagnetism.py
+follows the standards in https://docs.python.org/2/library/unittest.html
+"""
+
+import unittest
+
+from LOTlib.Examples.Magnetism.ComplexMagnetism import *
+class ComplexMagnetismTest(unittest.TestCase):
+	
+	# initialization that happens before each test is carried out
+	def setUp(self):
+		pass
+	
+	
+	
+	
+	
+	# function that is executed after each test is carried out
+	def tearDown(self):
+		pass
+	
+
+
+
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+
+
+
