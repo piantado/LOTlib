dbb02ef7b8d5fc0e857b0dabe699d406fd7ee293
sa- <samay.kapadia@gmail.com>
2014-08-01 13:46:30 -0400

    Fixed some names

--- LOTlib/Testing/Grammars/FiniteWithoutBvArgs.py
+++ LOTlib/Testing/Grammars/FiniteWithoutBvArgs.py
@@ -17,9 +17,9 @@ g.add_rule("V", "likes", None, 1.0)
 g.add_rule("V", "kills", None, 1.0)
 g.add_rule("V", "eats", None, 1.0)
 
-g.add_rule('AGE', '', ['NUMBER'], 1.0,  bv_type='INT', bv_args=None, bv_prefix='a')
-g.add_rule('INT', '20', None, 1.0)
-g.add_rule('INT', '22', None, 1.0)
+g.add_rule('AGE', '', ['NUMBER'], 1.0,  bv_type='NUMBER', bv_args=None, bv_prefix='a')
+g.add_rule('NUMBER', '20', None, 1.0)
+g.add_rule('NUMBER', '22', None, 1.0)
 
 for i in xrange(100):
 	print(g.generate())
