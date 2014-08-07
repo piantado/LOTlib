a341f947691f399c549caaaa411c15a10242181f
sa- <samay.kapadia@gmail.com>
2014-08-07 12:35:38 -0400

    Fixed grammar

--- LOTlibTest/Grammars/FiniteWithBVArgs.py
+++ LOTlibTest/Grammars/FiniteWithBVArgs.py
@@ -7,8 +7,7 @@ from LOTlib.Grammar import Grammar
 
 g = Grammar()
 
-g.add_rule("START", 'S', "A", 1.0, bv_type="P", bv_args=["B,B"], bv_prefix="p")
-g.add_rule("START", 'S', "A", 1.0, bv_type="P", bv_args=["A,B"], bv_prefix="p")
+g.add_rule("START", 'S', "A", 1.0, bv_type="A", bv_args=["B"], bv_prefix="p")
 g.add_rule("A", 'a', "B", 1.0)
 g.add_rule("A", 'a', "w", 1.0)
 g.add_rule("A", 'a', "x", 1.0)
