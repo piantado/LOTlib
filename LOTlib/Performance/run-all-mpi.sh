892c1be9e0afb48b36d72e0db4fa4ff96a249e3f
Steven Piantadosi <spiantado@gmail.com>
2014-08-11 20:35:34 -0400

    restore performance scripts

--- LOTlib/Performance/run-all-mpi.sh
+++ LOTlib/Performance/run-all-mpi.sh
@@ -1,10 +1,10 @@
 
 # A script to run all evaluation on a variety of models
 
-MPIEXEC="time nice -n 19 mpiexec -n 7" #"mpiexec -n 16"
-SAMPLES=10000
+MPIEXEC="time nice -n 19 mpiexec -n 16" #"mpiexec -n 16"
+SAMPLES=25000
 PRINT_EVERY=1000 #1000
-REPETITIONS=10 #25
+REPETITIONS=20 #25
 
 for m in Number Galileo RationalRules RegularExpression SimpleMagnetism
 do
@@ -12,11 +12,11 @@ do
 	$MPIEXEC python inference-evaluation.py --model=$m --samples=$SAMPLES --print-every=$PRINT_EVERY --repetitions=$REPETITIONS
 	R --no-save < plot-inference.R & 
 
-	#echo Running $m tempchain
-	#$MPIEXEC python tempchain-evaluation.py --model=$m --samples=$SAMPLES --print-every=$PRINT_EVERY --repetitions=$REPETITIONS
-	#R --no-save < plot-tempchain.R & 
+	echo Running $m tempchain
+	$MPIEXEC python tempchain-evaluation.py --model=$m --samples=$SAMPLES --print-every=$PRINT_EVERY --repetitions=$REPETITIONS
+	R --no-save < plot-tempchain.R & 
 
-	#echo Running $m proposal
-	#$MPIEXEC python proposal-evaluation.py --model=$m --samples=$SAMPLES --print-every=$PRINT_EVERY --repetitions=$REPETITIONS
-	#R --no-save < plot-proposal.R & 
+	echo Running $m proposal
+	$MPIEXEC python proposal-evaluation.py --model=$m --samples=$SAMPLES --print-every=$PRINT_EVERY --repetitions=$REPETITIONS
+	R --no-save < plot-proposal.R & 
 done
