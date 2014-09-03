9873d08d75ca22457db76b8706ece02cb7c1ee89
eric <ebigelow@u.rochester.edu>
2014-09-03 15:55:11 -0400

    Updated number tutorial: fixed math, added in MHSample code, added stuff to graph p(y in C | d) over all hypotheses

--- LOTlib/Examples/Tutorial/number.py
+++ LOTlib/Examples/Tutorial/number.py
@@ -5,32 +5,35 @@ from LOTlib.Hypotheses.LOTHypothesis import *
 from LOTlib.Evaluation.Eval import *
 from LOTlib.Miscellaneous import logsumexp, exp
 from math import log
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~ 1 : Wrapper class for hypothesis ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 class NumberGameExpression(LOTHypothesis):
 
-    def __init__(self, grammar, domain=100, args=['n'], **kwargs):
+    def __init__(self, grammar, domain=100, noise=0.9, args=['n'], **kwargs):
         LOTHypothesis.__init__(self, grammar, args=args, **kwargs)
         self.domain = domain
+        self.noise = noise
 
-    def compute_likelihood(self, data, noise=0.9):
-        """ Computes the likelihood of data. """
+    def compute_likelihood(self, data):
+        """ Likelihood of given data being produced by this hypothesis. """
 
         # Get domain subset that data would map to using hypothesis function
         subset = map(self, map(float, range(1, self.domain + 1)))
         subset = [item for item in subset if item <= self.domain]
 
-        # The likelihood of each element d in data will depend on whether d is in subset
+        # The likelihood for each datum 'd' will depend on whether d is in subset
         s = len(subset)
         self.likelihood = 0
-        
+
         for datum in data:
             if datum in subset:
-                self.likelihood += log(noise / s)
+                self.likelihood += log((self.noise /s) + ((1-self.noise) /self.domain))
             else:   # If datum not in hypo subset OR hypo subset is empty, use noise = (1-alpha)
-                self.likelihood += log((1-noise) / self.domain)
+                self.likelihood += log((1-self.noise) / self.domain)
 
         # This is required in all compute_likelihoods
         self.posterior_score = self.prior + self.likelihood
@@ -45,27 +48,77 @@ grammar = Grammar()
 grammar.add_rule('START', '', ['EXPR'], 1)
 grammar.add_rule('EXPR', 'times_', ['EXPR', 'EXPR'], 1)
 grammar.add_rule('EXPR', 'plus_', ['EXPR', 'EXPR'], 1)
-grammar.add_rule('EXPR', 'pow_', ['EXPR', 'EXPR'], 0.5)
+grammar.add_rule('EXPR', 'minus_', ['EXPR', 'EXPR'], 1)
+grammar.add_rule('EXPR', 'pow_', ['EXPR', 'EXPR'], 1)
 
 # Converted to digits so python can evaluate them
-grammar.add_rule('EXPR', 'n', None, 4)
+grammar.add_rule('EXPR', 'n', None, 10)
 for i in xrange(1, 10):
-    grammar.add_rule('EXPR', str(i), None, 0.10)
+    grammar.add_rule('EXPR', str(i), None, 4 - (3*i / 10))
 
 
 #~~~~~~~~~~~~~~~~~~~~~~~~ 3 : Generate hypotheses for data ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# Edit these parameters
+num_iters = 5000
+data = [3, 9, 17]
+domain = 100
+noise = 0.9
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
-num_iters = 100
-data = [2, 8, 16]
+'''
+from LOTlib.Inference.MetropolisHastings import MHSampler
+
+hypotheses = set()
+h0 = NumberGameExpression(grammar, domain=domain, noise=noise)
+for h in MHSampler(h0, data, steps=num_iters):
+    #print h   # with this you can see how hill climbing moves towards maxima
+    hypotheses.add(h)
+'''
 
 hypotheses = set()
 for _ in lot_iter(xrange(num_iters)):       # You can ctrl+C to exit this loop
-    t = NumberGameExpression(grammar)
+    t = NumberGameExpression(grammar, domain=domain, noise=noise)
     t.compute_posterior(data)               # Get prior, likelihood, posterior
     hypotheses.add(t)
 
-# the exp(posterior_score) is proportional to posterior probability, so we can estimate the normalizing constant Z like this:
+# exp(posterior_score) is proportional to posterior probability, so we estimate normalizing
+# constant Z like so:
 Z = logsumexp([h.posterior_score for h in hypotheses])
 
+
+#~~~~~~~~~~~~ printing prior, likelihood, & normalized posterior score ~~~~~~~~~~~~~~~~~#
 for h in sorted(hypotheses, key=lambda x: x.posterior_score):
     print h.prior, h.likelihood, exp(h.posterior_score - Z), h
+
+print str(num_iters) + ' number of iterations'
+print str(len(hypotheses)) + ' hypotheses in total'
+
+
+#~~~~~~~~~ graph p(y in C | d) as a function of y in domain (see figs 3,4,9) ~~~~~~~~~~~#
+# - NOTE: this does not calculate p(y in C | d);  change to describe what it does calc. #
+predictive_dist = [0.]*domain
+
+# Calculate chance of sampling for each item in domain
+for q in range(domain):
+    # Sum p(q|h) * p(h|D) for all hypotheses h
+    for h in hypotheses:
+        posterior = h.posterior_score - Z
+        likelihood = h.compute_likelihood([q+1])
+        predictive_dist[q] += exp(posterior + likelihood)
+
+fig, ax = plt.subplots()
+rects = plt.bar(range(1,domain+1), predictive_dist)
+
+plt.xlabel('Domain (from 1 to '+str(domain)+')')
+plt.ylabel('Probability of sampling this value')
+plt.title('This graph needs a title!')
+
+plt.show()
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ N O T E S ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+# assuming you run MH long enough to get best hypotheses, Z should be pretty accurate
+
+# it would be good to keep track of # of times you visit each state (hypothesis) during MH,
+# so that we can compare that to using the posterior scores; after a lot of iterations we should
+# see them converge
