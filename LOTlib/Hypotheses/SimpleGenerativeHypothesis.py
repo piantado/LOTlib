c13220c24d4f7acaecb4354079dee2b277b1d2cf
Steven Piantadosi <spiantado@gmail.com>
2014-10-25 18:03:35 -0400

    changes to make SimpleGenerativeHypothesis take function data

--- LOTlib/Hypotheses/SimpleGenerativeHypothesis.py
+++ LOTlib/Hypotheses/SimpleGenerativeHypothesis.py
@@ -1,41 +1,46 @@
 from LOTHypothesis import LOTHypothesis
-from copy import copy, deepcopy
 from collections import defaultdict
-from math import log, exp
+from math import log
+from LOTlib.Miscellaneous import Infinity
 
+
+def nicelog(x):
+    if x > 0.:
+        return log(x)
+    else:
+        return -Infinity
 
 class SimpleGenerativeHypothesis(LOTHypothesis):
     """
-            Here, the data is a hash from strings to counts.
+            Here, each data point is a mapping from input to a dict of counts of observed outputs,
+            where if you have K total outputs (sum(output.values()), that counts as K datapoints
+
             Each function eval results in a string, and the likelihood is the probability of generating the
             observed data. The function eval results in a thunk
 
             NOTE: FOR NOW, Insert/Delete moves are taken off because of some weirdness with the lambda thunks
     """
-    def __init__(self, grammar, nsamples=100, **kwargs):
+    def __init__(self, grammar, nsamples=100, sm=0.001, **kwargs):
         """ kwargs should include ll_sd """
-        assert kwargs.get('args', None) is None, "Cannot specify args to SimpleGenerativeHypothesis"
-        self.nsamples=nsamples
 
-        LOTHypothesis.__init__(self, grammar, args=[], **kwargs) # this is simple-generative since args=[] (a thunk)
+        self.nsamples = nsamples
+        self.sm = sm
+
+        LOTHypothesis.__init__(self, grammar,  **kwargs) # this is simple-generative since args=[] (a thunk)
 
     def compute_single_likelihood(self, datum):
         assert False, "Should not call this!"
 
-    def compute_likelihood(self, data, sm=0.001):
+    def compute_single_likelihood(self, datum):
         """
                 sm smoothing counts are added to existing bins of counts (just to prevent badness)
         """
         #print self
-        assert len(self.args) == 0, "Can only use SimpleGenerativeHypothesis on thunks!"
-        assert isinstance(data, dict), "Data supplied to SimpleGenerativeHypothesis must be a dict (function outputs to counts)"
+        assert isinstance(datum.output, dict), "Data supplied to SimpleGenerativeHypothesis must be a dict (function outputs to counts)"
 
         self.llcounts = defaultdict(int)
         for i in xrange(self.nsamples):
-            self.llcounts[ self() ] += 1
+            self.llcounts[ self(*datum.input) ] += 1
 
-        self.likelihood = sum([ data[k] * (log(self.llcounts[k] + sm)-log(self.nsamples + sm*len(data.keys())) ) for k in data.keys() ]) / self.likelihood_temperature
+        return sum([ datum.output[k] * (nicelog(self.llcounts[k] + self.sm)-nicelog(self.nsamples + self.sm*len(datum.output.keys())) ) for k in datum.output.keys() ]) / self.likelihood_temperature
 
-        self.posterior_score = self.likelihood + self.prior
-
-        return self.likelihood
