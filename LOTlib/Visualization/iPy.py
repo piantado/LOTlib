0b1f61433da71898131b795d746e7400173ef729
Eric B <ebigelow@u.rochester.edu>
2014-11-23 14:37:56 -0500

    Updated number game tutorial significantly.

---
+++ LOTlib/Visualization/iPy.py
@@ -0,0 +1,25 @@
+"""
+Tools for visualizing useful things.
+
+>>> import LOTlib.Visualization as viz
+
+"""
+from IPython.display import clear_output
+import sys
+
+
+def print_iters(i, num_iters, increm=20):
+    """Print incremental statements as we generate a large number of hypotheses.
+
+    TODO: should this be made into a more general version?
+
+    """
+    i += 1
+    j = 0
+    if i % (num_iters/increm) == 0:
+        j += 1
+        clear_output()
+        print '\nGenerating %i hypotheses...\n' % i
+        print '[' + '#'*j + '-'*(increm-j) + ']'
+        sys.stdout.flush()
+
