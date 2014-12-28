71b68700aaabc4f200c95e4f13797124c7da8c66
Eric B <ebigelow@u.rochester.edu>
2014-12-28 18:36:57 -0500

    Added individual probabilities grammar (finished) & LOT grammar (not done at all) to NumberGame.JoshModel.

---
+++ LOTlib/Examples/NumberGame/JoshModel/Model/Grammar.py
@@ -0,0 +1,169 @@
+"""
+
+In the model used in Josh Tenenbaum's thesis, the following rules were used . . .
+
+Math Rules:  p = lambda
+-----------------------
+- primes
+- odd
+- even
+- squares
+- cubes
+- 2^n, +37
+- 2^n, -32
+- *3
+- *4
+- *5
+- *6
+- *7
+- *8
+- *9
+- *10
+- *11
+- *12
+- end1
+- end2
+- end3
+- end4
+- end5
+- end6
+- end7
+- end8
+- end9
+- 2^n
+- 3^n
+- 4^n
+- 5^n
+- 6^n
+- 7^n
+- 8^n
+- 9^n
+- 10^n
+- range[1,100]
+
+Interval Rules:  p = (lambda - 1)
+---------------------------------
+- all range[n,m] subset of r[1,100], such that n <=m  (5,050 rules like this!)
+
+
+"""
+from LOTlib.Evaluation.Eval import register_primitive
+from LOTlib.Grammar import Grammar
+
+
+# ============================================================================================================
+# Mixture model grammar with math rules & interval rules as the 2 probabilities mixed.
+#
+#  This is identical to the model used in Josh Tenenbaum's thesis, except that (hopefully) we can do cooler
+#  stuff with optimizing the hyperparameter lambda (aka `lambda_mix`) sampling GrammarHypotheses!
+#
+
+def mix_grammar(lambda_mix=2./3.):
+
+    grammar = Grammar()
+    grammar.add_rule('START', '', ['MATH'], lambda_mix)
+    grammar.add_rule('START', '', ['INTERVAL'], (1-lambda_mix))
+
+    # --------------------------------------------------------------------------------------------------------
+    # Math rules (30-40 of these)
+
+    grammar.add_rule('MATH', 'mapset_', ['FUNC', 'DOMAIN_RANGE'], 1.)
+    grammar.add_rule('DOMAIN_RANGE', 'range_set_', ['1', '100'], 1.)
+    grammar.add_rule('FUNC', 'lambda', ['EXPR'], 1., bv_type='X', bv_p=1.)
+
+    # Odd numbers
+    grammar.add_rule('EXPR', 'plus_', ['ODD', str(1)], 1.)
+    grammar.add_rule('ODD', 'times_', ['X', str(2)], 1.)
+
+    # Primes
+    grammar.add_rule('EXPR', 'isprime_', ['X'], 1.)
+
+    # Squares, cubes
+    grammar.add_rule('EXPR', 'ipowf_', ['X', str(2)], 1.)
+    grammar.add_rule('EXPR', 'ipowf_', ['X', str(3)], 1.)
+
+    # { 2^n  -  32 }
+    register_primitive(lambda x: x if x in (2, 4, 8, 16, 64) else 0, name='pow2n_d32_')
+    grammar.add_rule('EXPR', 'pow2n_d32_', ['X'], 1.)
+    # { 2^n  &  37 }
+    register_primitive(lambda x: x if x in (2, 4, 8, 16, 32, 37, 64) else 0, name='pow2n_u37_')
+    grammar.add_rule('EXPR', 'pow2n_u37_', ['X'], 1.)
+
+    # [2,12] * n
+    for i in range(2, 13):
+        grammar.add_rule('EXPR', 'times_', ['X', str(i)], 1.)
+
+    # [2,10] ^ m
+    for i in range(2, 11):
+        grammar.add_rule('EXPR', 'ipowf_', [str(i), 'X'], 1.)
+
+    # Ends in [0,9]
+    for i in range(0, 10):
+        grammar.add_rule('EXPR', 'ends_in_', ['X', str(i)], 1.)
+
+    # --------------------------------------------------------------------------------------------------------
+    # Interval Rules (there will be ~5050 of these)
+
+    for n in range(1, 101):
+        for m in range(n, 101):
+            grammar.add_rule('INTERVAL', 'range_set_', [str(n), str(m)], 1.)
+
+    return grammar
+
+
+# ============================================================================================================
+# This has the same rules as the mixture model above, except each rule has an individual probability.
+#
+#  * With GrammarHypothesis, we can sample much more intricate models than the mixture model.
+#  * However, we also will have like 5000 rules to choose from now . . .
+#
+
+def individual_grammar():
+    grammar = Grammar()
+
+    # Math rules
+    grammar.add_rule('START', 'mapset_', ['FUNC', 'DOMAIN_RANGE'], 1.)
+    grammar.add_rule('DOMAIN_RANGE', 'range_set_', ['1', '100'], 1.)
+    grammar.add_rule('FUNC', 'lambda', ['EXPR'], 1., bv_type='X', bv_p=1.)
+
+    grammar.add_rule('EXPR', 'plus_', ['ODD', str(1)], 1.)
+    grammar.add_rule('ODD', 'times_', ['X', str(2)], 1.)
+    grammar.add_rule('EXPR', 'isprime_', ['X'], 1.)
+    grammar.add_rule('EXPR', 'ipowf_', ['X', str(2)], 1.)
+    grammar.add_rule('EXPR', 'ipowf_', ['X', str(3)], 1.)
+    register_primitive(lambda x: x if x in (2, 4, 8, 16, 64) else 0, name='pow2n_d32_')
+    grammar.add_rule('EXPR', 'pow2n_d32_', ['X'], 1.)
+    register_primitive(lambda x: x if x in (2, 4, 8, 16, 32, 37, 64) else 0, name='pow2n_u37_')
+    grammar.add_rule('EXPR', 'pow2n_u37_', ['X'], 1.)
+    for i in range(2, 13):
+        grammar.add_rule('EXPR', 'times_', ['X', str(i)], 1.)
+    for i in range(2, 11):
+        grammar.add_rule('EXPR', 'ipowf_', [str(i), 'X'], 1.)
+    for i in range(0, 10):
+        grammar.add_rule('EXPR', 'ends_in_', ['X', str(i)], 1.)
+
+    # Interval Rules
+    for n in range(1, 101):
+        for m in range(n, 101):
+            grammar.add_rule('START', 'range_set_', [str(n), str(m)], 1.)
+
+    return grammar
+
+
+# ============================================================================================================
+# This grammar should generate all the hypotheses of the grammars listed above (and others!) as subset of
+#  a larger language (possible w/ recursion).
+#
+#  We don't know what will happen so we can see, and compare it w/ our results above as well as Josh's
+#  original results.
+#
+
+def lot_grammar():
+    #
+    # TODO this grammar . . .
+    #
+    grammar = Grammar()
+
+    return grammar
+
+
