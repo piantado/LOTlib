d309f8c3c8bd6739e60ca59c8174b444689cd081
Eric B <ebigelow@u.rochester.edu>
2015-01-26 22:35:06 -0500

    Fixed is_prime & ends_in primitives.

--- LOTlib/Evaluation/Primitives/Number.py
+++ LOTlib/Evaluation/Primitives/Number.py
@@ -42,29 +42,26 @@ def ifU_(C,X):
 
 @primitive
 def ends_in_(n, d):
-    """Return true if number `n` ends with digit `d`, false otherwise. E.g. ends_in_(427, 7) == True"""
+    """Return `n` if it ends with digit `d`, 0 otherwise. E.g. ends_in_(427, 7) == 1"""
     if (n % 10) == d:
-        return True
+        return n
     else:
-        return False
+        return 0
 
 @primitive
 def isprime_(n):
     """Is `n` a prime number?"""
-    for a in range(2, int(n**0.5)+1):
-        if n % a == 0:
-            return False
-    return True
+    try:
+        for a in range(2, int(n**0.5)+1):
+            if n % a == 0:
+                return 0
+    except ValueError:
+        return 0
+    return n
 
 @primitive
 def primes_in_set_(A):
-    def isPrime(a):
-        for j in range(2, int(a**0.5)+1):
-            if a % j == 0:
-                return False
-        return True
-
-    return [n for n in A if isPrime(n)]
+    return [n for n in A if isprime_(n)]
 
 @primitive
 def in_domain_(A, domain):
