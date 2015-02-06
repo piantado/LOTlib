b135c074e831585f8ca68fceeba1552004833648
Eric B <ebigelow@u.rochester.edu>
2015-02-06 04:31:07 -0500

    Updated NumberGameHypothesis to include recent stuff.

--- LOTlib/Examples/NumberGame/JoshModel/Model/Hypothesis.py
+++ LOTlib/Examples/NumberGame/JoshModel/Model/Hypothesis.py
@@ -1,94 +1,33 @@
 
-from math import log
-import numpy as np
-from LOTlib.FunctionNode import FunctionNode
-from LOTlib.GrammarRule import BVUseGrammarRule
 from LOTlib.Hypotheses.GrammarHypothesisVectorized import GrammarHypothesisVectorized
-from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis, Infinity
-from LOTlib.Examples.NumberGame.NewVersion.Model import NumberGameHypothesis
 
 
 # ------------------------------------------------------------------------------------------------------------
-# NumberGameHypothesis simplified to resemble 'Josh Concepts' model
-
-class JoshConceptsHypothesis(LOTHypothesis):
-    """Domain hypothesis class for josh concepts number game with independent rule probs."""
-    def __init__(self, grammar, alpha=0.9, domain=100, **kwargs):
-        LOTHypothesis.__init__(self, grammar, args=[], **kwargs)
-        self.alpha = alpha
-        self.domain = domain
-
-    def compute_likelihood(self, data, **kwargs):
-        """Likelihood of specified data being produced by this hypothesis.
-
-        Probability is alpha if datum in set, (1-alpha) otherwise).
-
-        """
-        s = self()      # set of numbers corresponding to this hypothesis
-        error_p = (1.-self.alpha) / self.domain
-
-        def compute_single_likelihood(datum):
-            if s is not None and datum in s:
-                return log(self.alpha/len(s) + error_p)
-            else:
-                return log(error_p)
-
-        likelihoods = [compute_single_likelihood(d) for d in data]
-        self.likelihood = sum(likelihoods) / self.likelihood_temperature
-        self.update_posterior()
-        return self.likelihood
-
-class NoDoubleConstNGHypothesis(NumberGameHypothesis):
-    def compute_prior(self, recompute=False, vectorized=False):
-        """Compute the log of the prior probability.
-
-        """
-        # Re-compute the FunctionNode `self.value` generation probabilities
-        if recompute:
-            self.value.recompute_generation_probabilities(self.grammar)
-
-        # Compute this hypothesis prior
-        if self.value.count_subnodes() > self.maxnodes:
-            self.prior = -Infinity
-        else:
-            # Compute prior with either RR or not.
-            self.prior = self.value.log_probability() / self.prior_temperature
-
-        # Don't use this tree if we have 2 constants as children in some subnode
-        for fn in self.value.subnodes()[1:]:
-            if all([arg.name == '' and len(arg.args)==1 and isinstance(arg.args[0], FunctionNode) and arg.args[0].returntype=='OPCONST' for arg in fn.argFunctionNodes()]):
-                self.prior = -Infinity
-                break
-
-        self.update_posterior()
-        return self.prior
-
-
-# ------------------------------------------------------------------------------------------------------------
-# GrammarHypothesis classes
 
 class NoConstGrammarHypothesis(GrammarHypothesisVectorized):
-    def get_propose_idxs(self):
-        """
-        Skip 'Const' rules
+    """
+    Don't propose to rules with 'CONST' as the rhs variable.
 
-        """
+    """
+    def get_propose_idxs(self):
         proposal_indexes = range(self.n)
         nonterminals = self.grammar.nonterminals()
 
-        # Don't propose to constants!
+        # Don't propose to constants
         idxs, r = self.get_rules(rule_nt='CONST')
         for i in idxs:
             proposal_indexes.remove(i)
 
+        # Only rules with alternatives/siblings
         for nt in nonterminals:
-            # Only rules with alternatives/siblings
             idxs, r = self.get_rules(rule_nt=nt)
             if len(idxs) == 1:
                 proposal_indexes.remove(idxs[0])
 
         return proposal_indexes
 
+
+# ------------------------------------------------------------------------------------------------------------
 
 class MixtureGrammarHypothesis(GrammarHypothesisVectorized):
     """
--- LOTlib/Examples/NumberGame/NewVersion/Model/Hypothesis.py
+++ LOTlib/Examples/NumberGame/NewVersion/Model/Hypothesis.py
@@ -1,11 +1,12 @@
 
 from math import log
 from LOTlib.Evaluation.EvaluationException import TooBigException
-from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
+from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis, Infinity
 
 
 class NumberGameHypothesis(LOTHypothesis):
-    """Domain-specific wrapper class for hypotheses in the number game.
+    """
+    Domain-specific wrapper class for hypotheses in the number game.
 
     Hypotheses evaluate to a subset of integers in [1, domain].
 
@@ -16,38 +17,77 @@ class NumberGameHypothesis(LOTHypothesis):
         self.domain = domain
         self.value_set = None
 
-    def compute_single_likelihood(self, datum, s, error_p, updateflag=True):
-        if s is not None and datum in s:
-            return log(self.alpha/len(s) + error_p)
-        else:
-            return log(error_p)
+    def compute_prior(self, recompute=False, vectorized=False):
+        """Compute the log of the prior probability.
 
-    def compute_likelihood(self, data, updateflag=True, **kwargs):
+        """
+        # Re-compute the FunctionNode `self.value` generation probabilities
+        if recompute:
+            self.value.recompute_generation_probabilities(self.grammar)
+
+        # Compute this hypothesis prior
+        if self.value.count_subnodes() > self.maxnodes:
+            self.prior = -Infinity
+        else:
+            # Compute prior with either RR or not.
+            self.prior = self.value.log_probability() / self.prior_temperature
+
+        # Don't use this tree if we have 2 constants as children in some subnode
+        for fn in self.value.subnodes()[1:]:
+            if all([arg.name == '' and len(arg.args)==1 and isinstance(arg.args[0], FunctionNode)
+                                   and arg.args[0].returntype=='OPCONST' for arg in fn.argFunctionNodes()]):
+                self.prior = -Infinity
+                break
+
+        self.update_posterior()
+        return self.prior
+
+    def compute_likelihood(self, data, update_post=True, **kwargs):
         """Likelihood of specified data being produced by this hypothesis.
 
         If datum item not in set, it still has (1 - alpha) likelihood of being generated.
 
         """
         try:
-            s = self()      # set of numbers corresponding to this hypothesis
-                            # NOTE: This may be None if the hypothesis has too many nodes
+            s = self()      # Set of numbers corresponding to this hypothesis
         except OverflowError:
-            s = set()       # If our hypothesis call blows things up...   TODO handle this better!!
+            s = set()       # If our hypothesis call blows things up
         error_p = (1.-self.alpha) / self.domain
 
-        likelihoods = [self.compute_single_likelihood(d, s, error_p, updateflag=updateflag) for d in data]
+        def compute_single_likelihood(d):
+            """Internal method so we don't have to call self() each time."""
+            if s is not None and d in s:
+                return log(self.alpha/len(s) + error_p)
+            else:
+                return log(error_p)
+
+        likelihoods = [compute_single_likelihood(d) for d in data]
         likelihood = sum(likelihoods) / self.likelihood_temperature
-        if updateflag:
+        if update_post:
             self.likelihood = likelihood
             self.update_posterior()
         return likelihood
 
+    def compute_single_likelihood(self, d):
+        try:
+            s = self()
+        except OverflowError:
+            s = set()
+        error_p = (1.-self.alpha) / self.domain
+
+        if s is not None and d in s:
+            return log(self.alpha/len(s) + error_p)
+        else:
+            return log(error_p)
+
     def __call__(self, *args, **kwargs):
         if self.value_set is None:
-            try:                    # Sometimes self.value has too many nodes!
+            # Sometimes self.value has too many nodes
+            try:
                 value_set = LOTHypothesis.__call__(self)
             except TooBigException:
                 value_set = set()
+
             # Restrict our concept to being within our domain
             if isinstance(value_set, set):
                 value_set = [x for x in value_set if x <= self.domain]
