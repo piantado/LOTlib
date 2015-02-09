8db13c2e2a389a8538cedc87c39faf5dc9772988
Eric Bigelow <ebigelow@u.rochester.edu>
2015-02-09 15:04:39 -0500

    Removed old make_h0.

--- LOTlib/Examples/NumberGame/Model/Utilities.py
+++ LOTlib/Examples/NumberGame/Model/Utilities.py
@@ -3,20 +3,12 @@ import pickle
 from LOTlib import lot_iter
 from LOTlib.Utilities.FiniteBestSet import FiniteBestSet
 from LOTlib.Miscellaneous import logsumexp
-import Grammar as G, Hypothesis
-
 
 
 
 def normalizing_constant(hypotheses):
     """Estimate normalizing constant Z by logsumexp(posterior scores for all hypotheses)."""
     return logsumexp([h.posterior_score for h in hypotheses])
-
-
-def make_h0(grammar=G.grammar, **kwargs):
-    """Make initial NumberGameHypothesis."""
-    return Hypothesis.NumberGameHypothesis(grammar, **kwargs)
-
 
 def save_hypotheses(sampler, filename='numbergame_hypotheses.p'):
     hypotheses = set()
