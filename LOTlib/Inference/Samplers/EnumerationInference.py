9626f0fbcbdd516bfa1857a9b616567252e9b417
piantado <spiantado@gmail.com>
2015-06-05 10:37:13 -0400

    fixed off by 1 and num samples bugs

--- LOTlib/Inference/Samplers/EnumerationInference.py
+++ LOTlib/Inference/Samplers/EnumerationInference.py
@@ -12,8 +12,8 @@ class EnumerationInference(object):
     def __iter__(self):
         for i, t in enumerate(self.grammar.enumerate()):
 
-            if i > self.steps:
-                break
+            if i >= self.steps:
+                raise StopIteration
 
             h = self.make_h(value=t)
             h.compute_posterior(self.data)
@@ -29,11 +29,13 @@ if __name__ == "__main__":
     #data = generate_data(100)
     #from LOTlib.Examples.RegularExpression.Shared import grammar, make_h0, data
 
-    from LOTlib.Examples.Magnetism.Simple.Run import grammar, data, make_h0
+    from LOTlib.Examples.Magnetism.Simple import make_data, make_hypothesis
+    from LOTlib.Examples.Magnetism.Simple.Grammar import grammar
+
 
     #from LOTlib.Examples.RationalRules.Shared import grammar, data, make_h0
 
-    for h in break_ctrlc(EnumerationInference(grammar, make_h0, data)):
+    for h in break_ctrlc(EnumerationInference(grammar, make_hypothesis, make_data(), steps=10000)):
         print h.posterior_score, h
     
                 
