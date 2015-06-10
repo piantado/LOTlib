a6e3f62ece88c7a6a72f4e8d280482720bdc99da
piantado <spiantado@gmail.com>
2015-06-10 16:14:47 -0400

    changes to SimpleGenerativeHypothesis to make it an abstract class, not a LOTHypothesis

--- LOTlib/Examples/StochasticGrammarInduction/Model/Hypothesis.py
+++ LOTlib/Examples/StochasticGrammarInduction/Model/Hypothesis.py
@@ -1,6 +1,12 @@
 
 from LOTlib.Hypotheses.SimpleGenerativeHypothesis import SimpleGenerativeHypothesis
+from LOTlib.Hypotheses.LOTHypothesis import LOTHypothesis
 from Grammar import grammar
 
+class MyHypothesis(SimpleGenerativeHypothesis, LOTHypothesis):
+    def __init__(self, grammar, **kwargs):
+        SimpleGenerativeHypothesis.__init__(self)
+        LOTHypothesis.__init__(self, grammar, args=[''], **kwargs)
+
 def make_hypothesis():
-    return SimpleGenerativeHypothesis(grammar, args=[''] )
+    return MyHypothesis(grammar)
