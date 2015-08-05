c850d47b332ca5c16479fc65c38af559097f072d
gblackout <wblackout@163.com>
2015-08-04 20:20:12 -0400

    fixed a bug

--- LOTlib/Examples/FormalLanguageTheory/my_search_stp.py
+++ LOTlib/Examples/FormalLanguageTheory/my_search_stp.py
@@ -1,22 +1,19 @@
 import sys
 import codecs
 import itertools
-import LOTlib
 from optparse import OptionParser
 from pickle import dump
-import numpy as np
 import time
-
+import numpy as np
+import LOTlib
 from LOTlib.Miscellaneous import display_option_summary
 from LOTlib.MPI.MPI_map import is_master_process
 from LOTlib.Examples.Demo import standard_sample
 from LOTlib.Evaluation.Eval import register_primitive
 from LOTlib.Miscellaneous import flatten2str, logsumexp, qq
 from LOTlib.MPI.MPI_map import MPI_map
-from LOTlib.Examples.FormalLanguageTheory.RegularLanguage import Regularlanguage
-from LOTlib.Examples.FormalLanguageTheory.AnBn import AnBn
-from LOTlib.Examples.FormalLanguageTheory.AnB2n import AnB2n
-from LOTlib.Examples.FormalLanguageTheory.Dyck import Dyck
+from Model.Hypothesis import make_hypothesis
+from Language.Index import instance
 
 register_primitive(flatten2str)
 
@@ -28,51 +25,37 @@ def run(mk_hypothesis, lang, size, finite):
     if LOTlib.SIG_INTERRUPTED:
         return set()
 
-    return standard_sample(mk_hypothesis,
+    return standard_sample(lambda: mk_hypothesis(options.LANG),
                            lambda: lang.sample_data_as_FuncData(size, max_length=finite),
                            N=options.TOP_COUNT,
                            steps=options.STEPS,
                            show=False, save_top=None)
 
 
-def load_language(code):
-    exec ('from %s import make_hypothesis' % {
-        0: 'Regularlanguage',
-        1: 'AnBn',
-        2: 'AnB2n',
-        3: 'Dyck'
-    }[code])
-
-    return {
-        0: Regularlanguage(),
-        1: AnBn(),
-        2: AnB2n(),
-        3: Dyck()
-    }[code], make_hypothesis
-
-
 if __name__ == "__main__":
-    suffix = time.strftime('_%m%d_%H%M%S', time.localtime())
-
     # ========================================================================================================
     # Process command line arguments /
     # ========================================================================================================
     fff = sys.stdout.flush
     parser = OptionParser()
-    parser.add_option("--language", dest="LANG", type="int", default=0, help="code of a language")
+    parser.add_option("--language", dest="LANG", type="string", default=0, help="name of a language")
     parser.add_option("--steps", dest="STEPS", type="int", default=10000, help="Number of samples to run")
     parser.add_option("--top", dest="TOP_COUNT", type="int", default=20, help="Top number of hypotheses to store")
     parser.add_option("--finite", dest="FINITE", type="int", default=10, help="specify the max_length to make language finite")
+    parser.add_option("--name", dest="NAME", type="string", default='', help="name of file")
     (options, args) = parser.parse_args()
+
+    suffix = time.strftime('_' + options.NAME + '_%m%d_%H%M%S', time.localtime())
 
     # set the output codec -- needed to display lambda to stdout
     sys.stdout = codecs.getwriter('utf8')(sys.stdout)
     if is_master_process():
         display_option_summary(options); fff()
 
-    # you need to run 12 machine on that
-    DATA_RANGE = np.arange(1, 64, 6)
-    language, make_hypothesis = load_language(options.LANG)
+    # you need to run 20 machine on that
+    DATA_RANGE = np.arange(10, 400, 20)
+
+    language = instance(options.LANG)
     args = list(itertools.product([make_hypothesis], [language], DATA_RANGE, [options.FINITE]))
     # run on MPI
     results = MPI_map(run, args)
@@ -110,7 +93,7 @@ if __name__ == "__main__":
         # compute the normalizing constant. This is the log of the sum of the probabilities
         Z = logsumexp([h.posterior_score for h in hypotheses])
 
-        f = open('out_' + suffix, 'a')
+        f = open('out' + suffix, 'a')
         cnt = 0
         for h in hypotheses:
             #compute the number of different strings we generate
