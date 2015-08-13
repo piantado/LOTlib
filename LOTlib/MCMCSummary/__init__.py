c00236207b31f8116437ad9b0aba9388cced0637
steve piantadosi <spiantado@gmail.com>
2015-08-12 21:27:31 -0400

    updates to import

--- LOTlib/MCMCSummary/__init__.py
+++ LOTlib/MCMCSummary/__init__.py
@@ -1,4 +1,8 @@
 
 from TopN import TopN
 from Z import Z
+from Print import Print
+from Skip import Skip
+from Unique import Unique
+
 # from VectorSummary import VectorSummary ## We cannot import this because it breaks on the cluster -- not matplotlib
