527a7f7d7ed58c05467e4435e9d2a4927ab7e066
steve piantadosi <spiantado@gmail.com>
2015-08-14 22:54:11 -0400

    Updates to allow SampleStream with >> notation for chaining operations. See SampleStream's __main__ code for an example

--- LOTlib/MCMCSummary/SampleStream.py
+++ LOTlib/MCMCSummary/SampleStream.py
@@ -1,38 +1,91 @@
+import collections
 
 class SampleStream(object):
     """
     SampleStream is the basic class that others in this directory inherit from. It allows constructs
-    where we process and pass on samples, as in
-
-        pt = PosteriorTrace(plot_every=1000, window=False)
-        for h in break_ctrlc(pt(MHSampler(make_hypothesis, data)))
-            print h
-
-    or
-
-        for h in break_ctrlc(PosteriorTrace(MHSampler(make_hypothesis, data)))
-            print h
-
+    where we process and pass on samples. (See example below)
     """
 
     def __init__(self, generator=None):
-        self.__dict__.update(locals())
+        self.actions = [] # what do I do to things?
+        self.generator = generator
 
     def update(self, generator):
-        for x in generator:
-            self.add(x)
-            yield x
+        """ When given a generator, I will __enter__ and __exit__ (and on all of my actions)"""
+        with self:
+            for x in generator:
+                v = self.process(x)
+                if v is not None: # none means skip
+                    yield v
+
+    def process(self, x):
+
+        v = self.process_(x)
+        for a in self.actions:
+            if v is None: break
+            v = a.process(v)
+        return v
+
+    def process_(self, x):
+        """ The main processing step that must be overridden. If it returns x, the sample continues in the stream;
+            if it return None, the sample is deleted """
+        return x
+
+    def __rshift__(self, other):
+        """
+        Implements the shift operator. So
+
+        A >> B
+
+        calls A.__rshift__(B).
+
+        We have to return a new SampleStream object or else we can't chain >>
+        """
+
+        self.actions.append(other)
+
+        return self
 
     def __iter__(self):
+        assert self.generator is not None, "*** Cannot iter without a generator"
         for x in self.update(self.generator):
             yield x
 
-    def add(self, x):
-        """
-        Add a sample x to myself. Must be overwritten by subclasses
-        """
-        raise NotImplementedError
+    def __call__(self, x):
+        if isinstance(x, collections.Iterable):
+            self.update(x)
+        else:
+            self.process(x)
 
-    def __call__(self, generator):
-        for x in self.update(generator):
-            yield x
+    def __enter__(self):
+        for a in self.actions:
+            a.__enter__()
+
+    def __exit__(self, t, value, traceback):
+        """ I defaultly call all of my children's exits """
+        for a in self.actions:
+            a.__exit__(t, value, traceback)
+        return False
+
+
+if __name__ == "__main__":
+
+    from LOTlib import break_ctrlc
+    from LOTlib.Examples.Number.Model import make_hypothesis, make_data
+    from LOTlib.Inference.Samplers.MetropolisHastings import MHSampler
+    from __init__ import *
+
+    sampler = break_ctrlc(MHSampler(make_hypothesis(), make_data()))
+
+    tn = TopN(N=10)
+
+    for h in SampleStream(sampler) >> PosteriorTrace(plot_every=100) >> tn >> Save('hypotheses.pkl') \
+            >> Tee( Unique() >> PrintH(), Skip(30) >> Print(prefix="#\t")):
+        pass
+
+    tn.display()
+
+
+
+
+
