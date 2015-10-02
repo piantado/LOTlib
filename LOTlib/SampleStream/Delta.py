e3fd03115b9ac7beb91c3d3d209d9358fef06c93
Steven Piantadosi <spiantado@gmail.com>
2015-10-02 11:39:26 -0400

    _

---
+++ LOTlib/SampleStream/Delta.py
@@ -0,0 +1,17 @@
+
+from SampleStream import SampleStream
+
+class Delta(SampleStream):
+    """
+    Only show changes in the sample
+    """
+    def __init__(self, generator=None):
+        self.last = None
+        SampleStream.__init__(self, generator=generator)
+
+    def process_(self, x):
+        if self.last is not None and x == self.last:
+            return None
+        else:
+            self.last = x
+            return x
