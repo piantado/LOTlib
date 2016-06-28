a70126a1aaacbb7b69882a78e4e30fda002ce73c
Steven Piantadosi <spiantado@gmail.com>
2016-06-28 19:39:58 -0400

    updated compute_likelihood to correctly cache input->llcount mapping

--- LOTlib/Hypotheses/Likelihoods/StochasticFunctionLikelihood.py
+++ LOTlib/Hypotheses/Likelihoods/StochasticFunctionLikelihood.py
@@ -6,7 +6,7 @@
     NOTE: A very subtle error can occur if exceptions (like TooBigException) are caught in __call__, then ll_counts may never get set.
 """
 
-from LOTlib.Miscellaneous import attrmem, nicelog
+from LOTlib.Miscellaneous import attrmem, nicelog, Infinity
 from collections import Counter
 
 class StochasticFunctionLikelihood(object):
@@ -32,16 +32,32 @@ class StochasticFunctionLikelihood(object):
         ret.ll_counts = None # We must recompute these
         return ret
 
-    def compute_single_likelihood(self, datum, llcounts=None, nsamples=512, sm=0.1):
+    def compute_single_likelihood(self, datum, llcounts, sm=0.1):
         """
-                sm smoothing counts are added to existing bins of counts (just to prevent badness)
-                This can take an optiona llcounts in order to allow us to cache this externally
+                sm smoothing counts are added to existing bins of counts
         """
-        #print self
-        assert isinstance(datum.output, dict), "Data supplied to SimpleGenerativeHypothesis must be a dict (function outputs to counts)"
 
-        if llcounts is None: # compute if not passed in
-            llcounts = self.make_ll_counts(datum.input, nsamples=nsamples)
+        assert isinstance(datum.output, dict), "Data supplied to SimpleGenerativeHypothesis must be a dict of function outputs to counts"
+        assert llcounts is not None
 
-        return sum([ datum.output[k] * (nicelog(llcounts[k] + sm)-nicelog(nsamples + sm*len(datum.output.keys())) ) for k in datum.output.keys() ])
+        z = sum(llcounts.values)
+        return sum([ datum.output[k] * (nicelog(llcounts[k] + sm)-nicelog(sum(z) + sm*len(datum.output.keys())) ) for k in datum.output.keys() ])
 
+
+    @attrmem('likelihood')
+    def compute_likelihood(self, data, shortcut=-Infinity, nsamples=512, **kwargs):
+        # For each input, if we don't see its input (via llcounts), recompute it through simulation
+
+        seen = {} # hash of data inputs to llcounts for that input
+        ll = 0.0
+        for datum in data:
+
+            if datum.input not in seen:
+                seen[datum.input] = self.make_ll_counts(datum.input, nsamples=nsamples)
+
+            ll += self.compute_single_likelihood(datum, seen[datum.input] ) / self.likelihood_temperature
+
+            if ll < shortcut:
+                return -Infinity
+
+        return ll
