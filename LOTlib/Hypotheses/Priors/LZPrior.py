7e39b5c63da564db099ea49c12d8ff4264e549a4
piantado <spiantado@gmail.com>
2016-07-11 12:42:31 -0400

    updated LZPrior to use Mackay's implementation

--- LOTlib/Hypotheses/Priors/LZPrior.py
+++ LOTlib/Hypotheses/Priors/LZPrior.py
@@ -4,6 +4,12 @@
 # import lzstring
 
 from LOTlib.Miscellaneous import attrmem
+from LOTlib.Grammar import pack_string
+
+from LZutil.IntegerCodes import to_fibonacci as integer2bits # Use Mackay's Fibonacci code
+from LZutil.LZ2 import encode
+
+
 
 # compressor = lzstring.LZString()
 # # let's subtract off the min possible for some one character string
@@ -17,14 +23,19 @@ class LZPrior(object):
 
     @attrmem('prior')
     def compute_prior(self):
-
-        # first pack up
         s = self.grammar.pack_ascii(self.value)
+        bits = ''.join([integer2bits(1+pack_string.index(x)) for x in s ]) # 1+ since it must be positive
+        c = encode(bits, pretty=0)
 
-        # then compress with lzw
-        c = compressor.compressToBase64(s)
+        return -len(c)
 
-        # then take the compressed length
-        # NOTE: This length will probability only be accurate
-        # to within a char, or 8 bits
-        return -(len(c)-minL) / self.prior_temperature
+        # # first pack up
+        # s = self.grammar.pack_ascii(self.value)
+        #
+        # # then compress with lzw
+        # c = compressor.compressToBase64(s)
+        #
+        # # then take the compressed length
+        # # NOTE: This length will probability only be accurate
+        # # to within a char, or 8 bits
+        # return -(len(c)-minL) / self.prior_temperature
