ddbbb85a79e319ae6d3fe09610493973f14b1c1c
Josh Rule <rule@mit.edu>
2016-09-14 09:02:38 -0400

    add a Copy Proposal to LOTlib.Hypotheses.Proposers

--- LOTlib/Hypotheses/Proposers/__init__.py
+++ LOTlib/Hypotheses/Proposers/__init__.py
@@ -1,5 +1,3 @@
-
-
 class ProposalFailedException(Exception):
     """
     This gets raised when we have a proposal that can't succeed
@@ -9,3 +7,4 @@ class ProposalFailedException(Exception):
 from CopyRegenProposal import copy_regen_proposal
 from InsertDeleteProposal import insert_delete_proposal
 from RegenerationProposal import regeneration_proposal
+from CopyRegenProposal import copy_proposal
