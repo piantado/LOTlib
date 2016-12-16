1b72583f1de49c848b5a6f1c226abdcbdd9633c2
Jenna from the block! <jregiste@u.rochester.edu>
2016-12-16 16:01:43 -0500

    updating Model

--- LOTlib/Projects/Warker/Model.py
+++ LOTlib/Projects/Warker/Model.py
@@ -25,8 +25,7 @@ parser.add_option("-s", "--steps", dest="steps", type="int", help="steps for the
 parser.add_option("-c", "--chainz", dest="chains", type="int", help="number of chainz :P", default=25)
 parser.add_option("--terminals",dest="TERMINALS",help="which terminals are we using? one string")
 parser.add_option("--data",dest="DATA",help="what data is seen?")
-parser.add_option("--partition", dest="PARTITION", default=False, help="are we running partition MCMC?")
-#consider making a flag for partitionMCMC
+parser.add_option("-p","--partition", dest="PARTITION", default=False, help="are we running partition MCMC?")
 (options, args) = parser.parse_args()
 print options
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -43,15 +42,21 @@ register_primitive(LOTlib.Miscellaneous.flatten2str)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Parse the input: the terminals and the data seen
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 terminal_file = open(options.TERMINALS,"r")
 TERMINALS = []
 weights = []
 d = []
+
 for line in terminal_file:
     line = line.split(" ")
     TERMINALS.append(line[0])
     weights.append(line[1].strip("\n"))
 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Data
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 data_file = open(options.DATA,"r")
 
 for line in data_file:
@@ -59,6 +64,7 @@ for line in data_file:
 
 def make_data(d=d,size=options.datasize):
     output = {}
+    print d
     for val in d:
         output.update({val:size})
     return [FunctionData(input=[],output=output)]
@@ -67,6 +73,9 @@ def make_data(d=d,size=options.datasize):
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Grammar
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+@primitive
+def prob_():
+    return {True:log(.5),False:log(.5)}
 
 TERMINAL_WEIGHT = .1
 
@@ -80,7 +89,8 @@ grammar.add_rule('START', '', ['EXPR'], 1.0)
 grammar.add_rule('EXPR', 'sample_uniform_d', ['SET'], 1.) # this requires its own set for terminals
 grammar.add_rule('SET', '%s', ['DETTERMINAL'], 1.0)
 grammar.add_rule('SET', '%s+%s', ['DETTERMINAL', 'SET'], 1.0)
-#grammar.add_rule('SET', 'if_d', ['.5','SET','SET'],1.0)
+grammar.add_rule('PROB','prob_()', None,1.0)
+
 
 # Build up partitions before we have the terminals and strings
 # this way, partitions are mainly structural
@@ -96,6 +106,8 @@ for part in partitions:
 
 for t in TERMINALS:
     grammar.add_rule('DETTERMINAL', "'%s'"%t, None, TERMINAL_WEIGHT) # deterministic terminals participate in sets
+
+grammar.add_rule('EXPR', 'if_d', ['PROB','EXPR','EXPR'],1.0)
 
 grammar.add_rule('EXPR', 'cons_d', ['EXPR', 'EXPR'], 1.0/2.0)
 
@@ -140,20 +152,29 @@ class MyHypothesis(MultinomialLikelihoodLog, LOTHypothesis):
 def make_hypothesis():
     return MyHypothesis(grammar)
 
-def runme(x,datamt):
-    def make_data(d=d,size=options.datasize):
+def runme(d,x,datamt,partitions):
+    def make_data(d=d,size=datamt):
         output = {}
         for val in d:
             output.update({val:size})
         return [FunctionData(input=[],output=output)]
     print "Start: " + str(x) + " on this many: " + str(datamt)
     if options.PARTITION:
-        partitionMCMC(d)
+        partitionMCMC(make_data(),partitions)
     else:
         return standard_sample(make_hypothesis, make_data, show=True, N=100, save_top="topModel1.pkl", steps=100000)
+def howyoudoin(h):
+    doin = False
+    for key, values in h().iteritems():
+        print key
+        if len(key)>=3 and (key[2] == 'e' or key[2]== 'a' or key[2]== 'i'):
+            doin = True
+    return doin
 
 
-def partitionMCMC(data):
+
+def partitionMCMC(data,partitions):
+    print data
     topn= TopN(N=200, key="posterior_score")
     for p in break_ctrlc(partitions):
         print "Starting on partition ", p
@@ -161,10 +182,12 @@ def partitionMCMC(data):
         # Now we have to go in and fill in the nodes that are nonterminals
         v = grammar.generate(deepcopy(p))
 
-        h0 = MyHypothesis(grammar, value=v)
-        for h in break_ctrlc(MHSampler(h0, data, steps=10000, skip=0)):
+        #h0 = MyHypothesis(grammar, value=v)
+        h0= make_hypothesis()
+        print h0
+        for h in break_ctrlc(MHSampler(h0, data, steps=5000, skip=0)):
             # Show the partition and the hypothesis
-            #print h.posterior_score, p, h
+            print h.posterior_score, p, h, howyoudoin(h)
             topn.add(h)
     return set(topn)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -174,11 +197,14 @@ def partitionMCMC(data):
 if __name__ == "__main__":
 
     from LOTlib.Inference.Samplers.StandardSample import standard_sample
-
-    NCHAINS = 1
-
+    NCHAINS = 25
+    #standard_sample(make_hypothesis, make_data, show=True, N=100, save_top="topModel1.pkl", steps=100000)
+    from LOTlib.Primitives import *
+    import LOTlib.Miscellaneous
     from LOTlib.MPI import MPI_map
-    args=[[x, d] for d in range(1,10) for x in range(NCHAINS)]
+
+    #pass the data, data_amount, and chain number to MPI
+    args=[[d,damt, x, partitions] for damt in range(1,options.datasize) for x in range(NCHAINS)]
 
     myhyp=set()
     for top in MPI_map(runme, args):
